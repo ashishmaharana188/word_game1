334211d380e654b14e24cd20688676d65d471d5d
Ashish Kumar Maharana <86043589+Ashish-Maharana188@users.noreply.github.com>
2024-02-12 16:50:23 +0530

    Update hint.py

--- hint.py
+++ hint.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:8000"],
+    allow_origins=["https://word-game1-i6eqkaiu9-ashish-maharanas-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
