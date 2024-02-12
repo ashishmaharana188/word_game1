c5fbb1c05a12310b6827c37d57755764707ad4e2
Ashish Kumar Maharana <86043589+Ashish-Maharana188@users.noreply.github.com>
2024-02-12 16:27:03 +0530

    Update hint.py

--- hint.py
+++ hint.py
@@ -7,7 +7,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-origins = ["https://word-game1-bq4yqw7ea-ashish-maharanas-projects.vercel.app"]
+origins = ["https://word-game1-2rx6y696d-ashish-maharanas-projects.vercel.app"]
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
